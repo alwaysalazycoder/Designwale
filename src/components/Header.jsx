f57a67dd3867c5026949dd28d4b03519f7f7969b
alwaysalazycoder <gs314315316@gmail.com>
2023-03-03 20:27:20 +0530

    navbar setted

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,62 @@
-import React from 'react'
+import React, { useState } from "react";
+import Menu from "../components/Menu";
+import { BiMenuAltRight } from "react-icons/bi";
+import { CgClose } from "react-icons/cg";
+import logo from "../assests/logo.svg"
+
+import "../styles/header.css";
 
 const Header = () => {
+
+  const [showNavbar,setShowNavbar] = useState(false);
+  const closeIt = () => {
+    setShowNavbar(!showNavbar)
+  }
   return (
     <>
       <nav>
         <NavContent />
-        
+        <div className="trans">
+          <BiMenuAltRight onClick={closeIt}/>
+          <div>
+            <div className={`nav-menu ${showNavbar}`}>
+              <div className="close-button">
+                <div className="logo">
+                  <img src={logo} alt="logo" />
+                </div>
+                <div onClick={closeIt}>
+                  <CgClose />
+                </div>
+              </div>
+              <div className="stack">
+                <div>
+                  <a href="/#work" onClick={closeIt}>Work</a>
+                </div>
+                <div>
+                  <a href="/#work" onClick={closeIt}>Work</a>
+                </div>
+                <div>
+                  <a href="/#work" onClick={closeIt}>Work</a>
+                </div>
+                <div>
+                  <a href="/#work" onClick={closeIt}>Work</a>
+                </div>
+                <div>
+                  <a href="/#work" onClick={closeIt}>Work</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </nav>
     </>
-  )
+  );
 };
 
 const NavContent = () => (
   <>
     <h2>Satyam.</h2>
-    <div className='header-section'>
+    <div className="header-section">
       <a href="#home">Home</a>
       <a href="#work">Work</a>
       <a href="#services">Services</a>
@@ -22,14 +64,12 @@ const NavContent = () => (
       <a href="#timeline">Timeline</a>
       <a href="#testimonials">Testimonials</a>
       <a href="#contact">Contact Us</a>
-
     </div>
 
-    <a href="mailto:imintrouble0117@gmail.com" className='mailtoButton'>
+    <a href="mailto:imintrouble0117@gmail.com" className="mailtoButton">
       <button>E-mail</button>
     </a>
-
   </>
-)
+);
 
-export default Header
+export default Header;
