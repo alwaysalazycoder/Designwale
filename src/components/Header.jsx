4acf3d52ede3d773e152f8e194bc3dd7fa729666
alwaysalazycoder <gs314315316@gmail.com>
2023-03-04 01:02:48 +0530

    nav bar done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,52 +2,53 @@ import React, { useState } from "react";
 import { BiMenuAltRight } from "react-icons/bi";
 import { CgClose } from "react-icons/cg";
 import logo from "../assests/logo.svg"
+import { useRef } from "react";
 
 import "../styles/header.css";
 
 const Header = () => {
 
-  const [showNavbar,setShowNavbar] = useState(false);
-  const closeIt = () => {
-    setShowNavbar(!showNavbar)
-  }
+  const navRef = useRef();
+  const showNavbar = ()=>{
+    navRef.current.classList.toggle("responsive");
+  }  
   return (
     <>
       <nav>
         <NavContent />
-        <div className="trans">
-          <BiMenuAltRight onClick={closeIt}/>
-          <div>
-            <div className={`nav-menu ${showNavbar}`}>
-              <div className="close-button">
-                <div className="logo">
-                  <img src={logo} alt="logo" />
-                </div>
-                <div onClick={closeIt}>
-                  <CgClose />
-                </div>
-              </div>
-              <div className="stack">
-                <div>
-                  <a href="/#work" onClick={closeIt}>Work</a>
-                </div>
-                <div>
-                  <a href="/#work" onClick={closeIt}>Work</a>
-                </div>
-                <div>
-                  <a href="/#work" onClick={closeIt}>Work</a>
-                </div>
-                <div>
-                  <a href="/#work" onClick={closeIt}>Work</a>
-                </div>
-                <div>
-                  <a href="/#work" onClick={closeIt}>Work</a>
-                </div>
-              </div>
-            </div>
-          </div>
+
+        <div className="nav-btn">
+          <BiMenuAltRight onClick={showNavbar}/>
         </div>
       </nav>
+
+      <div className="responsive-navbar" ref={navRef}>
+        <div className="hstack">
+          <div className="nav-logo">
+            <img src="" alt="fuck" />
+          </div>
+          <div className="nav-close-btn">
+            <CgClose onClick={showNavbar}/>
+          </div>
+        </div>
+        <div className="stack">
+          <div>
+            <a href="/#contact" onClick={showNavbar}>Work</a>
+          </div>
+          <div>
+            <a href="/#contact" onClick={showNavbar}>Work</a>
+          </div>
+          <div>
+            <a href="/#contact" onClick={showNavbar}>Work</a>
+          </div>
+          <div>
+            <a href="/#contact" onClick={showNavbar}>Work</a>
+          </div>
+          <div>
+            <a href="/#contact">Work</a>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
